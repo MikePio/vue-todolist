50a2237fbd9665a1b01a0474d7d7a556a116bce1
MikePio <michelepiopilla@gmail.com>
2023-04-14 14:56:21 +0200

    Added link to show arrays in dom

--- script.js
+++ script.js
@@ -21,9 +21,34 @@ const {createApp} = Vue;
 createApp({
    data(){
       return{
-         title: 'To Do List'
+         title: 'To Do List',
+         tasks: [
+            {
+               text: 'Task 1',
+               done: false,
+            },
+            {
+               text: 'Task 2',
+               done: true,
+            },
+            {
+               text: 'Task 3',
+               done: false,
+            },
+            {
+               text: 'Task 4',
+               done: true,
+            }
+         ]
       }
+   },
+
+   methods:{
+
    }
+
+
+
 }).mount('#app')
 
 
